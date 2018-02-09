35e0af66ca9ab1c1d8b0449db0fc9ebe74efada5
Felix Kahmann <felix@kahmann.de>
2018-02-09 17:43:41 +0100

    add server_i2c.py

---
+++ server_i2c.py
@@ -0,0 +1,37 @@
+import smbus
+import time
+from flask import Flask
+
+app = Flask(__name__)
+
+# for RPI version 1, use "bus = smbus.SMBus(0)"
+bus = smbus.SMBus(1)
+
+# This is the address we setup in the Arduino Program
+address = 0x04
+
+def writeNumber(value):
+    bus.write_byte(address, value)
+    # bus.write_byte_data(address, 0, value)
+    return -1
+
+@app.route('/set-state/<int:port>-<int:state>')
+def setState(port, state):
+    if port < 2 | port > 13:
+        return "Invalid Port: ", port
+
+    if state != 0 & state != 1:
+        return "Invalid state: ", state
+
+    writeNumber(port)
+    writeNumber(state)
+
+    result = "Switched port ", port
+
+    if state == 1:
+        result = result, " on"
+    else:
+        result = result, " off"
+
+    return result
+
