0582a04819ce788f958a34052c2ae62f251c79c4
Felix Kahmann <felix@kahmann.de>
2018-02-12 19:18:41 +0100

    add some test setup

--- flask_server/main.py
+++ flask_server/main.py
@@ -1,4 +1,15 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
+
+@app.route('/test/')
+def get_test()
+    return render_template('test.html')
+
+@app.route('/send_data/', methods=['POST'])
+def post_data()
+    return redirect(url_for('success.html'))
+
+
+app.run(host="0.0.0.0", port=5001)
 
