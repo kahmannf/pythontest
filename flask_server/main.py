4fa85c65be9065411afb04a3582c0aac9fa61c2f
Felix Kahmann <felix@kahmann.de>
2018-02-12 19:27:20 +0100

    add some complexity

--- flask_server/main.py
+++ flask_server/main.py
@@ -2,13 +2,16 @@ from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
+counter = 0
+
 @app.route('/test/')
 def get_test():
     return render_template('test.html')
 
 @app.route('/post_data/', methods=['POST'])
 def post_data():
-    return redirect(url_for('success.html'))
+    counter = int(counter) + 1
+    return redirect(url_for('test'))
 
 
 app.run(host="0.0.0.0", port=5001)
