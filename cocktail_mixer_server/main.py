95c045804ff4b18457a8b65d4c9d34d6bf97e1f1
Felix Kahmann <felix@kahmann.de>
2018-02-14 10:54:58 +0100

    add functions "maintenance" and "login" for routes "/maintenance/" and "/login/"

--- cocktail_mixer_server/main.py
+++ cocktail_mixer_server/main.py
@@ -27,6 +27,21 @@ def show_recipes_filtered():
     return render_template('show_recipes.html', data=data)
 
 
+@app.route('/maintenance/', methods=['GET'])
+def maintenance():
+	if session['logged_in']:
+		return render_template('maintenance.html')
+	else:
+		return render_template('maintenance_login.html')
+
+@app.route('/login/', methods=['POST'])
+def login():
+	if request.form['pin'] != server_config['maintenance_pin']:
+		return render_template('maintenance_login.html', error='Invalid pin!')
+	else:
+		session['logged_in'] = True
+		return redirect(url_for('maintenance'))
+
 def main():
     ##data = Data()
 
